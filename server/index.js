60059511a1f6166300c34e28a2feb5dc1b6d4846
Young <hyangyoungs@gmail.com>
2023-07-24 10:56:32 -0500

    Connect to MongoDB cloud

--- server/index.js
+++ server/index.js
@@ -2,13 +2,26 @@ const express = require('express')
 const path = require("path");
 const app = express();
 const port = 9000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.use(express.static(path.join(__dirname, "../client/build")))
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-})
+    mongoose.connect(process.env.MONGO_URI)
+        .then(()=> {
+            console.log(`Example app listening on port http://localhost:${port}`);
+    }).catch((err) => {
+        console.log(`${err}`)
+    })
+        })
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
+
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
